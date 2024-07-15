c40dcdaffd7dfdab4ec7d4d6e9f7573e50a404f8
blessonnag <technicalguruji03011996@gmail.com>
2024-07-15 16:13:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("😷Welcome To SkyFish")
+
+import pandas as pd
+
+video_url = 'https://youtu.be/cBYLKtKYUAY?si=FsYBnj5SIs-neI8o'
+# Display the video
+st.video(video_url)
+video_url = 'https://youtu.be/2u2Z07ujyD8?si=gvSDs1z4nj9nZeRP'
+# Display the video
+st.video(video_url)
+
+
+
+
+
+
+
+
+
+
